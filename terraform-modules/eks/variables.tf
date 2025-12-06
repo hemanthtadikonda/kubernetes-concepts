3072755c708099ccdf3e93268ac11b58bba0ffb8
hemanth <hemanthtadikonda29@gmail.com>
2025-12-06 13:07:57 +0530

    Kubernetes operator

--- terraform-modules/eks/variables.tf
+++ terraform-modules/eks/variables.tf
@@ -8,7 +8,7 @@ variable "vpc_cidr" { type = string, default = "10.0.0.0/16" }
 variable "public_subnet_cidrs" { type = list(string), default = ["10.0.0.0/24","10.0.1.0/24"] }
 variable "private_subnet_cidrs" { type = list(string), default = ["10.0.10.0/24","10.0.11.0/24"] }
 
-variable "node_instance_type" { type = string, default = "c5.9xlarge" }
+variable "node_instance_type" { type = string, default = "t3.large" } //c5.9xlarge
 variable "node_min" { type = number, default = 1 }
 variable "node_desired" { type = number, default = 1 }
 variable "node_max" { type = number, default = 2 }
