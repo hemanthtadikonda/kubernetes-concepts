9ac5d6987e290910181066279889a20597382fae
hemanth <hemanthtadikonda29@gmail.com>
2025-12-06 13:26:46 +0530

    Kubernetes operator

--- terraform-modules/eks/main.tf
+++ terraform-modules/eks/main.tf
@@ -1,9 +1,18 @@
 terraform {
   required_version = ">= 1.3.0"
+
   required_providers {
-    aws = { source = "hashicorp/aws" version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.18"
+    }
   }
 }
+
 
 provider "aws" {
   region = var.region
