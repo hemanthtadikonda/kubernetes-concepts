05fe7781b9e7c6393e0974a7fc0fd48d39ea8600
hemanth <hemanthtadikonda29@gmail.com>
2025-12-09 10:26:34 +0530

    Kubernetes cluster addons

---
+++ terraform-modules/opensearch/main.tf
@@ -0,0 +1,63 @@
+resource "aws_opensearch_domain" "pimcore_global" {
+  domain_name = var.domain_name
+  engine_version = "OpenSearch_3.1" # choose appropriate version; adjust as needed
+
+  cluster_config {
+    instance_type = var.instance_type
+    instance_count = var.instance_count
+    zone_awareness_enabled = var.zone_awareness_enabled
+    zone_awareness_config {
+      availability_zone_count = var.availability_zone_count
+    }
+
+    dedicated_master_enabled = true
+    dedicated_master_type    = "m5.large.search"
+    dedicated_master_count   = 3
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_type = var.ebs_volume_type
+    volume_size = var.ebs_volume_size
+    # gp3 allows IOPS/throughput tuning if needed (optionally add iops and throughput)
+    # iops       = 3000
+    # throughput = 125
+  }
+
+  # VPC (no public access)
+  vpc_options {
+    subnet_ids = var.vpc_subnet_ids
+    security_group_ids = var.vpc_security_group_ids
+  }
+
+  access_policies = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": { "AWS": "*" },
+      "Action": "es:*",
+      "Condition": {
+        "IpAddress": { "aws:SourceIp": "10.0.0.0/8" }
+      },
+      "Resource": "arn:aws:es:${var.region}:${data.aws_caller_identity.current.account_id}:domain/${var.domain_name}/*"
+    }
+  ]
+}
+POLICY
+
+  domain_endpoint_options {
+    enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+  }
+  advanced_options = {
+    "rest.action.multi.allow_explicit_index" = "true"
+  }
+  tags = {
+    Project = "Hemanth-Admin"
+    Environment = "stage"
+  }
+}
+
+data "aws_caller_identity" "current" {}
---
+++ terraform-modules/opensearch/vars.tf
@@ -0,0 +1,57 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "domain_name" {
+  type    = string
+  default = "im-es-global-stage"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.small.search" //"m5.large.search"
+}
+
+variable "instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "zone_awareness_enabled" {
+  type    = bool
+  default = true
+}
+
+
+variable "availability_zone_count" {
+  type    = number
+  default = 2
+}
+
+variable "ebs_volume_size" {
+  type    = number
+  default = 30
+  description = "GiB"
+}
+
+variable "ebs_volume_type" {
+  type    = string
+  default = "gp3"
+}
+
+variable "vpc_subnet_ids" {
+  type    = list(string)
+  default = ["subnet-0875410ab2506d634","subnet-0a0966e1ba6be380a",]
+}
+
+variable "vpc_security_group_ids" {
+  type    = list(string)
+  default = ["sg-0baa986d8ebb5eeda"]
+}
+
+## Optional: S3 bucket for manual snapshots (if you want manual backups stored in your S3)
+#variable "snapshot_bucket_name" {
+#  type    = string
+#  default = ""
+#}
