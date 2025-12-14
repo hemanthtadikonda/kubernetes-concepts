5c611600e28faa600d2b9e1eea3d0baf27fc5633
hemanth <hemanthtadikonda29@gmail.com>
2025-12-14 09:26:49 +0530

    Kubernetes cluster & AWS

--- terraform-modules/vpc/vpc.tf
+++ terraform-modules/vpc/vpc.tf
@@ -4,10 +4,10 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs              =  ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets  =  ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets   =  ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets =  ["10.0.11.0/24", "10.0.12.0/24","10.0.13.0/24" ]
+  azs              =  ["eu-west-1a", "eu-west-1b" ]
+  private_subnets  =  ["10.0.1.0/24", "10.0.2.0/24" ]
+  public_subnets   =  ["10.0.101.0/24", "10.0.102.0/24" ]
+  database_subnets =  ["10.0.11.0/24", "10.0.12.0/24" ]
 
   enable_nat_gateway = true
   one_nat_gateway_per_az = true
